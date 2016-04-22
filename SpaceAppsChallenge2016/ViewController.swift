617f6e9e49ef7c41e04922d9ebcca0c09d0c5007
wirawan <kucingsunter@gmail.com>
2016-04-22 22:08:33 +0200

    Initial Commit

---
+++ SpaceAppsChallenge2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceAppsChallenge2016
+//
+//  Created by wirawan sanusi on 4/22/16.
+//  Copyright © 2016 Protogres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
